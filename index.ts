961a064121ea4ac17441fa692678c4ffa9da185f
Анастасия <nastunechka2008@gmail.com>
2026-09-18 20:57:31 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Application, 
+Request, Response } from 'express';
+
+const app: Application = express(); 
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res:
+Response) => { 
+    res.send('Hello from Currency Converter!');
+});
+app.listen(PORT, () => {
+     console.log(`Server is running on http://localhost:${PORT}`); 
+});
